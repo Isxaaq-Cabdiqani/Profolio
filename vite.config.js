58700281ce40c89bb848f8fb55275bd467593670
isxaaq cabdiqani <isxaaq206@gmail.com>
2025-12-10 23:45:26 +0300

    fixing deployment (1)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const repositoryName = "Telecom-Academy-Profolio";
+const repositoryName = "Profolio";
 
 export default defineConfig({
   plugins: [react()],
